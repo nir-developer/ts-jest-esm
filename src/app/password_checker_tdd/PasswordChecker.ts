98993610700443721c79a79e68b2c322d9c59cff
nir-developer <nir100.developer@gmail.com>
2023-12-14 09:31:13 +0200

    password-checker: iter 2 - requirement to return error messages worked and tested!

--- src/app/password_checker_tdd/PasswordChecker.ts
+++ src/app/password_checker_tdd/PasswordChecker.ts
@@ -1,9 +1,38 @@
-export class PasswordChecker {
-  public checkPassword(password: string) {
-    if (password.length < 8) return false;
-    if (password.toLowerCase() === password) return false;
-    if (password.toUpperCase() === password) return false;
+export enum PasswordErrors {
+  SHORT = "Password is too short!",
+  NO_UPPER_CASE = "Upper case letter required!",
+  NO_LOWER_CASE = "Lower case letter required!",
+}
 
-    return true;
+export interface CheckResult {
+  valid: boolean;
+  reasons: PasswordErrors[];
+}
+
+export class PasswordChecker {
+  public checkPassword(password: string): CheckResult {
+    let reasons = [];
+    let valid;
+    if (password.length < 8) reasons.push(PasswordErrors.SHORT);
+    if (password.toLowerCase() === password)
+      reasons.push(PasswordErrors.NO_UPPER_CASE);
+    if (password.toUpperCase() === password)
+      reasons.push(PasswordErrors.NO_LOWER_CASE);
+
+    valid = reasons.length === 0;
+
+    return {
+      valid,
+      reasons,
+    };
   }
+
+  //AFTER ITER 1 - ALL 3 REQUIREMENTS WORKED AND TESTED!
+  // public checkPassword(password: string) {
+  //   if (password.length < 8) return false;
+  //   if (password.toLowerCase() === password) return false;
+  //   if (password.toUpperCase() === password) return false;
+
+  //   return true;
+  // }
 }
--- src/test/password_checker_tdd/PasswordChecker.spec.ts
+++ src/test/password_checker_tdd/PasswordChecker.spec.ts
@@ -1,4 +1,8 @@
-import { PasswordChecker } from "../../app/password_checker_tdd/PasswordChecker";
+import {
+  PasswordChecker,
+  PasswordErrors,
+  CheckResult,
+} from "../../app/password_checker_tdd/PasswordChecker";
 describe("PasswordChecker Project", () => {
   let sut: PasswordChecker;
 
@@ -10,38 +14,103 @@ describe("PasswordChecker Project", () => {
   it("Password with less than 8 chars is invalid", () => {
     const testPassword = "1234567";
 
-    expect(sut.checkPassword(testPassword)).toBe(false);
+    const actual = sut.checkPassword(testPassword);
+
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.SHORT);
+
+    console.log(actual.reasons);
+    //INTERFACE HAS NO TYPE??
+    console.log(typeof actual);
+
+    //BEFORE ITER 2:  REFACTORING TO THE TYPE AND ENUM
+    //expect(sut.checkPassword(testPassword)).toBe(false);
   });
 
   it("Password with at least 8 chars is valid", () => {
+    const testPassword = "abcA1234";
+
+    const actual = sut.checkPassword(testPassword);
+
+    expect(actual.valid).toBe(true);
+    expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
+
+    console.log(actual.reasons);
+    //////ITER 1 - OK!
     //Changed for requirement 2 for uppercase!! bad
     //const testPassword = "12345678";
     //const testPassword = "AAAA11111";
     //CHANGE FOR ITER 1 - REQUIRE 3
-    const testPassword = "abcA1234";
 
-    expect(sut.checkPassword(testPassword)).toBe(true);
+    //expect(sut.checkPassword(testPassword)).toBe(true);
   });
 
   //ITER 1 - REQUIREMENT 2
   it("Password with no uppercase is invalid", () => {
     const testPassword = "ab778787878";
+    const actual = sut.checkPassword(testPassword);
 
-    expect(sut.checkPassword(testPassword)).toBe(false);
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+
+    console.log(actual.reasons);
+
+    //ITER 1 - OK !
+    // const testPassword = "ab778787878";
+    // expect(sut.checkPassword(testPassword)).toBe(false);
   });
 
   it("Password with uppercase is valid", () => {
-    //CHANGE FOR ITER 1 - REQUIRE 3
-    // const testPassword = "A1111111";
     const testPassword = "A111111a";
 
-    expect(sut.checkPassword(testPassword)).toBe(true);
+    const actual = sut.checkPassword(testPassword);
+
+    expect(actual.valid).toBe(true);
+
+    expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
+
+    console.log(actual.reasons);
+
+    ///ITER 1 - OK
+    //CHANGE FOR ITER 1 - REQUIRE 3
+    // const testPassword = "A1111111";
+    // const testPassword = "A111111a";
+
+    // expect(sut.checkPassword(testPassword)).toBe(true);
   });
 
   //ITER 1 - REQUIREMENT 3
   it("Password without lower is invalid", () => {
     const testPassword = "A1111111";
 
-    expect(sut.checkPassword(testPassword)).toBe(false);
+    const actual = sut.checkPassword(testPassword);
+
+    expect(actual.valid).toBe(false);
+    expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
+    console.log(actual.reasons);
+
+    //AFTER ITER 1 - OK
+    // const testPassword = "A1111111";
+    // expect(sut.checkPassword(testPassword)).toBe(false);
+  });
+
+  it("Complex Password is valid", () => {
+    const testPassword = "23aaaABs";
+
+    const actual = sut.checkPassword(testPassword);
+
+    expect(actual.valid).toBe(true);
+
+    //expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
+    expect(actual.reasons).toEqual([]);
+
+    console.log(actual.reasons);
+
+    ///ITER 1 - OK
+    //CHANGE FOR ITER 1 - REQUIRE 3
+    // const testPassword = "A1111111";
+    // const testPassword = "A111111a";
+
+    // expect(sut.checkPassword(testPassword)).toBe(true);
   });
 });
